8804dd6b2165a4478d73b57a4ccace62c655158e
ryo-takahashi <ryo.takahashi.work@gmail.com>
2023-09-14 00:49:32 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World");
+import { WebcastPushConnection } from "tiktok-live-connector";
+
+let tiktokUsername = "fantasyonline_official";
+
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+tiktokLiveConnection
+  .connect()
+  .then((state) => {
+    console.info(`Connected to roomId ${state.roomId}`);
+  })
+  .catch((err) => {
+    console.error("Failed to connect", err);
+  });
+
+tiktokLiveConnection.on("chat", (data) => {
+  console.log(
+    `${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`
+  );
+});
+
+tiktokLiveConnection.on("gift", (data) => {
+  console.log(`${data.uniqueId} (userId:${data.userId}) sends ${data.giftId}`);
+});
